63433033a3776e7a4e5fc23a2d8ad8029a0e6c83
Jamie D. <45217971+jamie-dang-n@users.noreply.github.com>
2026-06-03 00:07:45 -0700

    Adding additional terraform destroy cleanup and persistent state with s3 bucket (for CI/CD)

--- versions.tf
+++ versions.tf
@@ -7,4 +7,11 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  # s3 backend for maintaining terraform state for github actions
+  backend "s3" {
+    bucket = "minecraft-tf-state"
+    key    = "minecraft/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
