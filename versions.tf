0ed368f324550570344a8969bcd358456640ab34
Jamie D. <45217971+jamie-dang-n@users.noreply.github.com>
2026-06-03 00:12:44 -0700

    unique bucket name

--- versions.tf
+++ versions.tf
@@ -10,7 +10,7 @@ terraform {
 
   # s3 backend for maintaining terraform state for github actions
   backend "s3" {
-    bucket = "minecraft-tf-state"
+    bucket = "minecraft-tf-state_dangjam"
     key    = "minecraft/terraform.tfstate"
     region = "us-east-1"
   }
